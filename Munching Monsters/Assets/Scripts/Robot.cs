ffa669841ff4cc4ce04921be6003c07666893fca
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-01-18 14:55:31 +0000

    Update Robot.cs

--- Munching Monsters/Assets/Scripts/Robot.cs
+++ Munching Monsters/Assets/Scripts/Robot.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
+    // Get a reference to the GameObject of the current monster
     public GameObject currentMonster;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Start the coroutine which works as the robot
         StartCoroutine(RobotClick());
     }
 
@@ -18,19 +20,25 @@ public class Robot : MonoBehaviour
         
     }
 
+    // This coroutine will run every second on a loop while the game is running
     public IEnumerator RobotClick()
     {
+        // Wait for 1 second
         yield return new WaitForSeconds(1);
 
+        // Set the current monster GameObject
         currentMonster = GameObject.FindGameObjectWithTag("Monster");
 
+        // If the monster exists and the robot level is greater than 0, then call the RobotClicked() function
         if (currentMonster != null && gVar.robotLvl != 0)
         {
             currentMonster.GetComponent<Monster>().RobotClicked();
         }
 
+        // Set the current monster GameObject to null
         currentMonster = null;
 
+        // Call the coroutine again
         StartCoroutine(RobotClick());
     }
 }
