ed761009dafbfad93c95bdeb4f9b1554bbdc07ea
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-01-18 10:43:51 +0000

    Update Bird.cs

--- Munching Monsters/Assets/Scripts/Bird.cs
+++ Munching Monsters/Assets/Scripts/Bird.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    // A float to control the speed that the bird moves at
     private float speed = 2;
 
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.left * speed * Time.deltaTime); 
+        // While the bird is active then every frame move it towards the left by the speed variable based on the framerate
+        transform.Translate(Vector3.left * speed * Time.deltaTime); 
     }
 
+    // When the object becomes invisible (meaning it can't be seen by the camera), then set the object to inactive
     void OnBecameInvisible()
     {
         this.gameObject.SetActive(false);
