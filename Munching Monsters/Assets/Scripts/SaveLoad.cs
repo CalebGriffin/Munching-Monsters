8a8bba187fd30bd4b1896d2f64771f47f41f0423
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-01-18 14:55:34 +0000

    Update SaveLoad.cs

--- Munching Monsters/Assets/Scripts/SaveLoad.cs
+++ Munching Monsters/Assets/Scripts/SaveLoad.cs
@@ -4,33 +4,37 @@ using UnityEngine;
 
 public class SaveLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-       // REMEMBER TO REMOVE
-       PlayerPrefs.DeleteAll();
+   // Start is called before the first frame update
+   void Start()
+   {
+      // REMEMBER TO REMOVE
+      // Deletes all of the Player Prefs
+      PlayerPrefs.DeleteAll();
 
-       gVar.cookieSize = PlayerPrefs.GetFloat("cookieSize", 1); 
-       gVar.cookieFlavours = PlayerPrefs.GetFloat("cookieFlavours", 1); 
-       gVar.robotArmy = PlayerPrefs.GetFloat("robotArmy", 1); 
-       gVar.robotLvl = PlayerPrefs.GetInt("robotLvl", 1); 
-       gVar.monsterLvl = PlayerPrefs.GetInt("monsterLvl", 1); 
-       gVar.gold = PlayerPrefs.GetFloat("gold", 0);
-       gVar.monstersSpawned = PlayerPrefs.GetInt("monstersSpawned", 0);
-       gVar.cookieCost = PlayerPrefs.GetFloat("cookieCost", 10 * gVar.cookieSize);
-       gVar.flavourCost = PlayerPrefs.GetFloat("flavourCost", 20 * gVar.cookieFlavours);
-       gVar.robotCost = PlayerPrefs.GetFloat("robotCost", 10 * gVar.robotArmy);
-       gVar.upgradeCost = PlayerPrefs.GetFloat("upgradeCost", 20 * gVar.robotLvl);
-       gVar.totalMonstersSpawned = PlayerPrefs.GetInt("totalMonstersSpawned", 0);
-    }
+      // Sets up all of the different global variables when the game starts
+      gVar.cookieSize = PlayerPrefs.GetFloat("cookieSize", 1); 
+      gVar.cookieFlavours = PlayerPrefs.GetFloat("cookieFlavours", 1); 
+      gVar.robotArmy = PlayerPrefs.GetFloat("robotArmy", 1); 
+      gVar.robotLvl = PlayerPrefs.GetInt("robotLvl", 1); 
+      gVar.monsterLvl = PlayerPrefs.GetInt("monsterLvl", 1); 
+      gVar.gold = PlayerPrefs.GetFloat("gold", 0);
+      gVar.monstersSpawned = PlayerPrefs.GetInt("monstersSpawned", 0);
+      gVar.cookieCost = PlayerPrefs.GetFloat("cookieCost", 10 * gVar.cookieSize);
+      gVar.flavourCost = PlayerPrefs.GetFloat("flavourCost", 20 * gVar.cookieFlavours);
+      gVar.robotCost = PlayerPrefs.GetFloat("robotCost", 10 * gVar.robotArmy);
+      gVar.upgradeCost = PlayerPrefs.GetFloat("upgradeCost", 20 * gVar.robotLvl);
+      gVar.totalMonstersSpawned = PlayerPrefs.GetInt("totalMonstersSpawned", 0);
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-       //PlayerPrefs.SetInt("cookieSize", gVar.cookieSize); 
-       //PlayerPrefs.SetInt("cookieFlavours", gVar.cookieFlavours); 
-       //PlayerPrefs.SetInt("robotArmy", gVar.robotArmy); 
-       //PlayerPrefs.SetInt("robotLvl", gVar.robotLvl); 
-       //PlayerPrefs.SetInt("monsterLvl", gVar.monsterLvl); 
-    }
+   // Update is called once per frame
+   void Update()
+   {
+      // NOT IN USE
+      // This would have updated some of the variables every frame but this was not efficient
+      //PlayerPrefs.SetInt("cookieSize", gVar.cookieSize); 
+      //PlayerPrefs.SetInt("cookieFlavours", gVar.cookieFlavours); 
+      //PlayerPrefs.SetInt("robotArmy", gVar.robotArmy); 
+      //PlayerPrefs.SetInt("robotLvl", gVar.robotLvl); 
+      //PlayerPrefs.SetInt("monsterLvl", gVar.monsterLvl); 
+   }
 }
